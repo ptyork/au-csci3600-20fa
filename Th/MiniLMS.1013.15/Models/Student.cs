1d795550ac43d6bbf44a3acd134bc29599a4057b
Paul York <paul@yorkfamily.com>
2020-10-12 22:11:01 -0400

    MiniLMS For Tu/Th

---
+++ Th/MiniLMS.1013.15/Models/Student.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiniLMS.Models
+{
+    public class Student
+    {
+        //
+        // DATA PROPERTIES
+        //
+
+        [Key]
+        public int StudentId { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string LastName { get; set; }
+
+        public string CurrentMajor { get; set; }
+
+        public string CurrentMinor { get; set; }
+
+        public DateTime? GraduationDate { get; set; }
+
+        public StudentCategory Category { get; set; }
+
+        public StudentStates Status { get; set; }
+
+
+        //
+        // NAVIGATION PROPERTIES
+        //
+
+        // one to many relationship with Enrollments
+        public List<Enrollment> Enrollments { get; set; }
+
+
+        //
+        // OTHER PROPERTIES AND METHODS
+        //
+
+        [NotMapped]
+        public string FullName
+        {
+            get
+            {
+                return FirstName + " " + LastName;
+            }
+        }
+
+        [NotMapped]
+        public bool HasGraduated
+        {
+            get
+            {
+                return GraduationDate != null;
+            }
+        }
+
+        [NotMapped]
+        public bool IsActive
+        {
+            get
+            {
+                return !HasGraduated &&
+                       Status <= StudentStates.AcademicProbation;
+            }
+        }
+
+        //
+        // Enumerated values (these are stored in the Database as ints
+        //
+
+        public enum StudentCategory
+        {
+            Undergraduate = 0,
+            Graduate = 1
+        }
+
+        public enum StudentStates
+        {
+            GoodStanding = 0,
+            AcademicProbation = 1,
+            Inactive = 9
+        }
+
+    }
+}
---
+++ Tu/MiniLMS.1013.15/Models/Section.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiniLMS.Models
+{
+    public class Section
+    {
+        //
+        // DATA PROPERTIES
+        //
+
+        [Key]
+        public int SectionId { get; set; }
+
+        [Required]
+        [MaxLength(4)]
+        public string Semester { get; set; }
+
+        [Required,MaxLength(1)] // NO!!!!
+        public string SectionLetter { get; set; }
+
+        [Range(0,999)]
+        public int? MaxStudents { get; set; } = 30;
+
+        // Good idea to have "flags" here to avoid the need to actually delete
+        // data from the database. This is especially bad with so many other
+        // tables related to this one since it would require a LOT of cascading.
+        public bool IsActive { get; set; } = false;
+
+        public DateTime? StartDate { get; set; }
+
+
+        //
+        // NAVIGATION PROPERTIES
+        //
+
+        // Many to one relationship to Instructors
+        public Instructor Instructor { get; set; }
+        // Just because let's specify the FK Field
+        // Also because it's "best practice"
+        // Oh, also because we want it nullable
+        public int? InstructorId { get; set; }
+
+        // many to one relationship to Courses
+        public Course Course { get; set; }
+        // still sepcifying the FK field, but NOT nullable because it's required
+        public int CourseId { get; set; }
+
+        // one to many relationship to Enrollments
+        public List<Enrollment> Enrollments { get; set; }
+
+    }
+}
